6f84752c255750868f437b451cbbd5ae2789031d
angelotavara07 <angelotavara07@gmail.com>
2026-05-04 13:48:53 -0500

    Actualizando los archivos

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use App\Models\Order;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class)
+                ->withPivot(['quantity', 'price'])
+                    ->withTimestamps();
+    }
+}
